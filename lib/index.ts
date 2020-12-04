00b1dc16eeb6c7c322af2be53145f8b155b90049
Thyiad <1520583107@qq.com>
2020-12-05 00:40:36 +0800

    feat: update

---
+++ lib/index.ts
@@ -0,0 +1,13 @@
+import _Bar from './Charts/Bar';
+import * as _ChartTool from './Charts/tool';
+import _UploadBtn from './Upload/UploadBtn';
+import _UploadFileList from './Upload/FileList';
+import _IconFont from './IconFont/index';
+import _PageLoading from './PageLoading/index';
+
+export const Bar = _Bar;
+export const ChartTool = _ChartTool;
+export const UploadBtn = _UploadBtn;
+export const UploadFileList = _UploadFileList;
+export const IconFont = _IconFont;
+export const PageLoading = _PageLoading;
